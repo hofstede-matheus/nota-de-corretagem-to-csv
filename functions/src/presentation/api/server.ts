7966348c5150a4ca967599745f60ee38bb414664
hofstede-matheus <hofstede.matheus@gmail.com>
2022-10-20 23:29:09 -0300

    chore: fix importing

--- functions/src/presentation/api/server.ts
+++ functions/src/presentation/api/server.ts
@@ -1,5 +1,5 @@
-import * as express from 'express'
-import * as cors from 'cors'
+import express from 'express'
+import cors from 'cors'
 import * as BrokerageNoteToCsv from '../../services/BrokerageNoteToCsv'
 import filesUpload from './middleware/middleware'
 
